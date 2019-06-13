6c27e56ed1ffcdfff05ee014eb38ce951e8fd9bd
John Leonardo <jdleonardo5@icloud.com>
2019-06-12 20:03:06 -0700

    newConvo() cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,52 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.newConvo = functions.firestore.document('chats/{conversationId}').onCreate((snap, context) => {
+    //reference to db
+    var db = admin.firestore();
+
+    //object representing the new document
+    const doc = snap.data();
+
+    //document ID
+    let documentID = context.params.conversationId;
+
+    //get both party members UIDs
+    const members = doc.parties;
+
+    //empty dictionary
+    var res = {};
+
+    //promises array
+    var promises = [];
+
+    //iterate through conversation members
+    members.forEach(function(member) {
+        var currentPromise = db.collection("users").doc(member).get().then(snapshot => {
+            //this user
+            const userDoc = snapshot.data();
+            //check account type
+            if (userDoc.accountType == 2) {
+                //influencer
+                const name = userDoc.firstName + " " + userDoc.lastName;
+                res[member] = name;
+            } else if (userDoc.accountType == 1) {
+                //brand
+                const name = userDoc.name;
+                res[member] = name;
+            }
+        });
+        //push to promises array
+        promises.push(currentPromise);
+    });
+
+    //all done
+    return Promise.all(promises).then(function(values) {
+        //reference to current document in firestore
+        let messageRef = db.collection("chats").doc(documentID)
+
+        //return promise
+        return messageRef.set(res, {merge: true});
+    });
+});
