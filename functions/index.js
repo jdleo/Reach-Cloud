17ec328c71a6e86d2d5fdabe2de0aceed2eb83c0
John Leonardo <jdleonardo5@icloud.com>
2019-06-17 12:23:36 -0700

    orderUpdate() preliminary functionality

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,49 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
+exports.orderUpdate = functions.firestore.document('orderTree/{orderId}').onUpdate((change, context) => {
+    //reference to db
+    var db = admin.firestore();
+
+    //object representing the document (after change)
+    const change = change.after.data();
+
+    //status field
+    const status = change.status;
+
+    //parties
+    const parties = change.parties;
+
+    //check if status is 'completed'
+    if (status === "completed") {
+        //check if no reviews have been left yet
+        if (!(change[`review_${parties[0]}`]) && !(change[`review_${parties[1]}`])) {
+            return db.collection('socials').where('username', '==', change.username).get().then(function(snap) {
+                //check if has orders field
+                if (snap[0].orders) {
+                    //reference to current document in firestore
+                    let socialRef = db.collection("socials").doc(snap[0].id)
+
+                    //return promise
+                    return socialRef.set(["orders": 1], { merge: true });
+                } else {
+                    //reference to current document in firestore
+                    let socialRef = db.collection("socials").doc(snap[0].id)
+
+                    //orders
+                    let orders = snap[0].orders;
+
+                    //return promise
+                    return socialRef.set(["orders": orders + 1], { merge: true });
+                }
+            });
+        }
+    }
+
+    //complete
+    return;
+});
+
 exports.newConvo = functions.firestore.document('chats/{conversationId}').onCreate((snap, context) => {
     //reference to db
     var db = admin.firestore();
@@ -47,6 +90,6 @@ exports.newConvo = functions.firestore.document('chats/{conversationId}').onCrea
         let messageRef = db.collection("chats").doc(documentID)
 
         //return promise
-        return messageRef.set(res, {merge: true});
+        return messageRef.set(res, { merge: true });
     });
 });
